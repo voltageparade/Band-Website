365f61285fbd0213814e592c2aaff8ffda762b9f
Alyssa <alyssaphelps3@gmail.com>
2026-08-20 21:12:45 +0100

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Footer from './elements/Footer';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom';
-import { HomePage } from './pages/HomePage'
+import { HomePage } from './pages/HomePage';
 import { Header } from './elements/Header';
 
 function App() {
